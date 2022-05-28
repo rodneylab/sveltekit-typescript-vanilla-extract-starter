979884ac6a85c24a6ae43fae7a0dc1646bd915c5
Rodney Lab <codesign@rodneylab.com>
2022-05-28 14:03:08 +0100

    fix: 💫 switch to hsl colours

--- src/lib/styles/themes/dark.css.ts
+++ src/lib/styles/themes/dark.css.ts
@@ -3,16 +3,16 @@ import { themeVars } from './theme.css';
 
 export const darkTheme = createTheme(themeVars, {
   colour: {
-    primary: '#457b9d',
-    secondary: '#32021f',
-    alternative: '#a8dadc',
-    background: '#1d3557',
-    text: '#fed766',
-    textAlternative: '#815e5b',
-    textSecondary: '#32021f',
-    shadow: '#815e5b22',
-    accent: '#fed766',
-    buttonText: '#fed766',
-    buttonBackground: '#1d3557',
+    primary: 'hsl(203deg 39% 44%)', // wedgewood
+    secondary: 'hsl(324deg 92% 10%)', // jacaranda
+    alternative: 'hsl(182deg 43% 76%)', // aqua island
+    background: 'hsl(215deg 50% 23%)', // cello
+    text: 'hsl(45deg 99% 70%)', // goldenrod
+    textAlternative: 'hsl(5deg 17% 43%)', // russett
+    textSecondary: 'hsl(324deg 92% 10%)',
+    shadow: 'hsl(5deg 17% 43% / 0.34)', // russett
+    accent: 'hsl(45deg 99% 70%)',
+    buttonText: 'hsl(45deg 99% 70%)',
+    buttonBackground: 'hsl(215deg 50% 23%)',
   },
 });
--- src/lib/styles/themes/light.css.ts
+++ src/lib/styles/themes/light.css.ts
@@ -3,16 +3,16 @@ import { themeVars } from '$lib/styles/themes/theme.css';
 
 export const lightTheme = createTheme(themeVars, {
   colour: {
-    primary: '#457b9d',
-    secondary: '#a8dadc',
-    alternative: '#433633',
-    background: '#fcfdfd', // 'hsl(203,20%,99%)',
-    text: '#1d3557',
-    textAlternative: '#815e5b',
-    textSecondary: '#433633',
-    accent: '#fff',
-    shadow: '#43363321',
+    primary: 'hsl(203deg 39% 44%)', // wedgewood
+    secondary: 'hsl(182deg 43% 76%)', // aqua island
+    alternative: 'hsl(11deg 14% 23%)', // woody brown
+    background: 'hsl(180deg 20% 99%)', // porcelain
+    text: 'hsl(215deg 50% 23%)', // cello
+    textAlternative: 'hsl(5deg 17% 43%)', // russett
+    textSecondary: 'hsl(11deg 14% 23%)',
+    accent: 'hsl(0deg 0% 100%)', // white
+    shadow: 'hsl(11deg 14% 23%) / 0.33',
     buttonText: 'hsl(203,20%,99%)',
-    buttonBackground: '#1d3557',
+    buttonBackground: 'hsl(215deg 50% 23%)',
   },
 });
